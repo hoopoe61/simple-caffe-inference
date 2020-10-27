99972b1e283e4bbbaaaf5f5901fdfbf14dc435ad
dengshunge <dengshunge2013@163.com>
2020-10-27 14:15:53 +0800

    删除部分文件

--- include/caffe/data_transformer.hpp
+++ include/caffe/data_transformer.hpp
@@ -7,25 +7,27 @@
 #include "caffe/common.hpp"
 #include "caffe/proto/caffe.pb.h"
 
-namespace caffe {
+namespace caffe
+{
 
-/**
+  /**
  * @brief Applies common transformations to the input data, such as
  * scaling, mirroring, substracting the image mean...
  */
-template <typename Dtype>
-class DataTransformer {
- public:
-  explicit DataTransformer(const TransformationParameter& param, Phase phase);
-  virtual ~DataTransformer() {}
+  template <typename Dtype>
+  class DataTransformer
+  {
+  public:
+    explicit DataTransformer(const TransformationParameter &param, Phase phase);
+    virtual ~DataTransformer() {}
 
-  /**
+    /**
    * @brief Initialize the Random number generations if needed by the
    *    transformation.
    */
-  void InitRand();
+    void InitRand();
 
-  /**
+    /**
    * @brief Applies the transformation defined in the data layer's
    * transform_param block to the data.
    *
@@ -35,9 +37,9 @@ class DataTransformer {
    *    This is destination blob. It can be part of top blob's data if
    *    set_cpu_data() is used. See data_layer.cpp for an example.
    */
-  void Transform(const Datum& datum, Blob<Dtype>* transformed_blob);
+    void Transform(const Datum &datum, Blob<Dtype> *transformed_blob);
 
-  /**
+    /**
    * @brief Applies the transformation defined in the data layer's
    * transform_param block to a vector of Datum.
    *
@@ -47,11 +49,11 @@ class DataTransformer {
    *    This is destination blob. It can be part of top blob's data if
    *    set_cpu_data() is used. See memory_layer.cpp for an example.
    */
-  void Transform(const vector<Datum> & datum_vector,
-                Blob<Dtype>* transformed_blob);
+    void Transform(const vector<Datum> &datum_vector,
+                   Blob<Dtype> *transformed_blob);
 
 #ifdef USE_OPENCV
-  /**
+    /**
    * @brief Applies the transformation defined in the data layer's
    * transform_param block to a vector of Mat.
    *
@@ -61,10 +63,10 @@ class DataTransformer {
    *    This is destination blob. It can be part of top blob's data if
    *    set_cpu_data() is used. See memory_layer.cpp for an example.
    */
-  void Transform(const vector<cv::Mat> & mat_vector,
-                Blob<Dtype>* transformed_blob);
+    void Transform(const vector<cv::Mat> &mat_vector,
+                   Blob<Dtype> *transformed_blob);
 
-  /**
+    /**
    * @brief Applies the transformation defined in the data layer's
    * transform_param block to a cv::Mat
    *
@@ -74,10 +76,10 @@ class DataTransformer {
    *    This is destination blob. It can be part of top blob's data if
    *    set_cpu_data() is used. See image_data_layer.cpp for an example.
    */
-  void Transform(const cv::Mat& cv_img, Blob<Dtype>* transformed_blob);
-#endif  // USE_OPENCV
+    void Transform(const cv::Mat &cv_img, Blob<Dtype> *transformed_blob);
+#endif // USE_OPENCV
 
-  /**
+    /**
    * @brief Applies the same transformation defined in the data layer's
    * transform_param block to all the num images in a input_blob.
    *
@@ -88,17 +90,17 @@ class DataTransformer {
    *    This is destination blob, it will contain as many images as the
    *    input blob. It can be part of top blob's data.
    */
-  void Transform(Blob<Dtype>* input_blob, Blob<Dtype>* transformed_blob);
+    void Transform(Blob<Dtype> *input_blob, Blob<Dtype> *transformed_blob);
 
-  /**
+    /**
    * @brief Infers the shape of transformed_blob will have when
    *    the transformation is applied to the data.
    *
    * @param datum
    *    Datum containing the data to be transformed.
    */
-  vector<int> InferBlobShape(const Datum& datum);
-  /**
+    vector<int> InferBlobShape(const Datum &datum);
+    /**
    * @brief Infers the shape of transformed_blob will have when
    *    the transformation is applied to the data.
    *    It uses the first element to infer the shape of the blob.
@@ -106,8 +108,8 @@ class DataTransformer {
    * @param datum_vector
    *    A vector of Datum containing the data to be transformed.
    */
-  vector<int> InferBlobShape(const vector<Datum> & datum_vector);
-  /**
+    vector<int> InferBlobShape(const vector<Datum> &datum_vector);
+    /**
    * @brief Infers the shape of transformed_blob will have when
    *    the transformation is applied to the data.
    *    It uses the first element to infer the shape of the blob.
@@ -116,19 +118,19 @@ class DataTransformer {
    *    A vector of Mat containing the data to be transformed.
    */
 #ifdef USE_OPENCV
-  vector<int> InferBlobShape(const vector<cv::Mat> & mat_vector);
-  /**
+    vector<int> InferBlobShape(const vector<cv::Mat> &mat_vector);
+    /**
    * @brief Infers the shape of transformed_blob will have when
    *    the transformation is applied to the data.
    *
    * @param cv_img
    *    cv::Mat containing the data to be transformed.
    */
-  vector<int> InferBlobShape(const cv::Mat& cv_img);
-#endif  // USE_OPENCV
+    vector<int> InferBlobShape(const cv::Mat &cv_img);
+#endif // USE_OPENCV
 
- protected:
-   /**
+  protected:
+    /**
    * @brief Generates a random integer from Uniform({0, 1, ..., n-1}).
    *
    * @param n
@@ -136,19 +138,18 @@ class DataTransformer {
    * @return
    *    A uniformly random integer value from ({0, 1, ..., n-1}).
    */
-  virtual int Rand(int n);
+    virtual int Rand(int n);
 
-  void Transform(const Datum& datum, Dtype* transformed_data);
-  // Tranformation parameters
-  TransformationParameter param_;
+    void Transform(const Datum &datum, Dtype *transformed_data);
+    // Tranformation parameters
+    TransformationParameter param_;
 
+    shared_ptr<Caffe::RNG> rng_;
+    Phase phase_;
+    Blob<Dtype> data_mean_;
+    vector<Dtype> mean_values_;
+  };
 
-  shared_ptr<Caffe::RNG> rng_;
-  Phase phase_;
-  Blob<Dtype> data_mean_;
-  vector<Dtype> mean_values_;
-};
+} // namespace caffe
 
-}  // namespace caffe
-
-#endif  // CAFFE_DATA_TRANSFORMER_HPP_
+#endif // CAFFE_DATA_TRANSFORMER_HPP_
